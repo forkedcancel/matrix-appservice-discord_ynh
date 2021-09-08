f901ec65dccef2a8c1c65a3681233c000994ddcd
Florent <florent.git@zeteo.me>
2021-09-08 19:51:22 +0200

    First commit

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,16 +5,27 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="postgresql"
+# Node version
+NODEJS_VERSION=14
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+install_node_deps() {
+    pushd "$final_path"
+        ynh_use_nodejs
+        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production npm install
+        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production npm run build
+    popd
+}
 
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
+run_discordas() {
+    ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production node build/src/discordas.js -r -u "http://localhost:$port" -c config.yaml
+}
 
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
+setup_final_path_rights() {
+    chmod 750 "$final_path"
+    chmod -R o-rwx "$final_path"
+    chown -R $app:$app "$final_path"
+}
