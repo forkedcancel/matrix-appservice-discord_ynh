6692352d6e0031928be23875c5887ebc8839aef6
William Rose <william.rose@siriusxm.com>
2023-07-08 09:19:40 -0700

    Move typescript installation to common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,6 +16,7 @@ install_node_deps() {
   pushd "$final_path"
   ynh_use_nodejs
   ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn build
+  ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH node install typescript
   popd
 }
 
