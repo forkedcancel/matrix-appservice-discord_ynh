789427d0b538a34cdde35eb8229dcf429d4ddb19
William Rose <william.rose@siriusxm.com>
2023-07-07 20:09:36 -0700

    Attempting to switch to yarn

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,19 +13,18 @@ NODEJS_VERSION=16
 # PERSONAL HELPERS
 #=================================================
 install_node_deps() {
-    pushd "$final_path"
-        ynh_use_nodejs
-        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production npm install
-        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production npm run build
-    popd
+  pushd "$final_path"
+  ynh_use_nodejs
+  ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn build
+  popd
 }
 
 run_discordas() {
-    ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production node build/src/discordas.js -r -u "http://localhost:$port" -c config.yaml
+  ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH NODE_ENV=production node build/src/discordas.js -r -u "http://localhost:$port" -c config.yaml
 }
 
 setup_final_path_rights() {
-    chmod 750 "$final_path"
-    chmod -R o-rwx "$final_path"
-    chown -R $app:$app "$final_path"
+  chmod 750 "$final_path"
+  chmod -R o-rwx "$final_path"
+  chown -R $app:$app "$final_path"
 }
