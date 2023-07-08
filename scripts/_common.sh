e974beb7295800807f27af648d4cbb9d1e17d7e4
William Rose <william.rose@siriusxm.com>
2023-07-08 09:47:34 -0700

    Whoops again

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,8 +15,8 @@ NODEJS_VERSION=16
 install_node_deps() {
   pushd "$final_path"
   ynh_use_nodejs
-  ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn build
   ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH npm install -g typescript
+  ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn build
   popd
 }
 
