0397757e340f679701830ea7a2b7cbbedf95ddb5
William Rose <william.rose@siriusxm.com>
2023-07-08 09:59:57 -0700

    Explicitly adding type defs for node and mocha before calling yarn build

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,6 +16,7 @@ install_node_deps() {
   pushd "$final_path"
   ynh_use_nodejs
   ynh_exec_warn_less sudo env $ynh_node_load_PATH npm install -g typescript
+  ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn add -D @types/node @types/mocha
   ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH yarn build
   popd
 }
